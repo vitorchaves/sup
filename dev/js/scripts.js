825e7a30fef9021b46051bed3546d57f969a7f1c
Vitor <vhxchaves@gmail.com>
2021-05-12 10:58:38 -0300

    Ajuste do carousel de clientes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -180,17 +180,8 @@ $(document).ready(function () {
       350: {
         items: 2
       },
-      575: {
-        items: 3
-      },
-      768: {
-        items: 4
-      },
-      1000: {
-        items: 5
-      },
       1200: {
-        items: 6,
+        items: 3,
         nav: true
       }
     }
